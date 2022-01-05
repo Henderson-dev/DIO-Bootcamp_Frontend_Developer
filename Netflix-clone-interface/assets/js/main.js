048e37a4c62271a7f300bcc2e39e4c6ec412a1fd
Henderson-dev <teconet.arte@gmail.com>
2022-01-05 14:40:07 -0300

    Add efects in itens of carrousel

--- Netflix-clone-interface/assets/js/main.js
+++ Netflix-clone-interface/assets/js/main.js
@@ -4,13 +4,13 @@ $('.owl-carousel').owlCarousel({
     nav:false,
     responsive:{
         0:{
-            items:1
+            items:1.5
         },
         600:{
-            items:3
+            items:3.5
         },
         1000:{
-            items:5
+            items:5.5
         }
     }
 });
